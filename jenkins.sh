e66196805e0ea2f26aca6fa3455e5dd2620dd830
Gujjar Aditya <gujjaraditya66@gmail.com>
2025-05-18 18:11:32 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 sudo apt update -y
 sudo apt install openjdk-17-jdk -y
-sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
-sudo apt install jenkins -y
-service jenkins start
-cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 #chmod +x jenkins.sh
 #./jenkins.sh
