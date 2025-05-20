357be7c0011947c863824516224c5cebe7b0286e
Gujjar Aditya <gujjaraditya66@gmail.com>
2025-05-20 23:18:26 +0530

    Update kubectl.sh

--- kubernetes/kubectl.sh
+++ kubernetes/kubectl.sh
@@ -1,5 +1,6 @@
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+# run as ubuntu user 
+sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 kubectl version --client
