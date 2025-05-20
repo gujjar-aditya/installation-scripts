db6a875a803467e377ff351f5533da4004c2fd90
Gujjar Aditya <gujjaraditya66@gmail.com>
2025-05-20 21:19:18 +0530

    Create kubectl.sh

---
+++ kubernetes/kubectl.sh
@@ -0,0 +1,3 @@
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
